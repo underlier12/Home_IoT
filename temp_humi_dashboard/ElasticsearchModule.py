7e6eaef49c1d7fac2956d774aac77a9f8e897207
wychoi <underlier12@gmail.com>
2021-01-03 17:38:39 +0900

    fix: Correct port of elasticsearch

--- temp_humi_dashboard/ElasticsearchModule.py
+++ temp_humi_dashboard/ElasticsearchModule.py
@@ -5,7 +5,7 @@ import json
 
 class ElasticsearchModule:
     def __init__(self, index):
-        self.es = Elasticsearch()
+        self.es = Elasticsearch('https://localhost:29200')
         self.index = index
 
     def check_existing_index(self):
