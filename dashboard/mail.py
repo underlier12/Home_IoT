6a7859d836f8f1782cd002df6fb8e7868436c098
wychoi <underlier12@gmail.com>
2021-01-10 23:46:07 +0900

    feat: Update mail parser for water charge

--- dashboard/mail.py
+++ dashboard/mail.py
@@ -2,11 +2,14 @@ import imaplib
 import base64
 import os
 import email
+import datetime
+
+from bs4 import BeautifulSoup
 
 class EmailModule():
     def __init__(self):
         self.user = 'user@naver.com'
-        self.pswd = 'pawd'
+        self.pswd = 'pswd'
 
     def login(self):
         mail = imaplib.IMAP4_SSL("imap.naver.com", 993)
@@ -16,7 +19,21 @@ class EmailModule():
     def read_content(self, charge_type, mail):
         print('read_content')
         mail.select()
-        _, data = mail.search(None, 'ALL')
+        end_date = datetime.date.today()
+        start_date = datetime.timedelta(3)
+        range_date = (end_date - start_date).strftime("%d-%b-%Y")
+        print(end_date)
+        print(start_date)
+        print(range_date)
+
+        # _, data = mail.search(None, \
+        #     '(FROM "abc@nicepay.co.kr" \
+        #     SINCE \"{}\")'.format(datetime.date.today()))
+
+        _, data = mail.search(None, \
+            '(SENTSINCE {})'.format(range_date), \
+            '(FROM "arisuyogm@i121.seoul.go.kr")')
+
         mail_ids = data[0]
         id_list = mail_ids.split()
         latest_id_list = id_list[-1:]
@@ -30,7 +47,12 @@ class EmailModule():
                 msg = msg.get_payload(0)
             
             content = msg.get_payload(decode=True)
-            print(content.decode('utf-8'))
+            content = content.decode('utf-8')
+            # print(content.decode('utf-8'))
+
+            soup = BeautifulSoup(content, 'html.parser')
+            spans = soup.find_all('span')
+            print(spans[4].get_text().replace(',', '').replace('원', ''))
 
 
 def main():
