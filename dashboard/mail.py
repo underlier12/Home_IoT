97f502024d54a60142f9cbfdaa5815065efaf0fa
wychoi <underlier12@gmail.com>
2021-01-11 22:15:23 +0900

    feat: Add getting electric charge part with division

--- dashboard/mail.py
+++ dashboard/mail.py
@@ -3,6 +3,7 @@ import base64
 import os
 import email
 import datetime
+import re
 
 from bs4 import BeautifulSoup
 
@@ -16,29 +17,53 @@ class EmailModule():
         mail.login(self.user, self.pswd)
         return mail
 
-    def read_content(self, charge_type, mail):
-        print('read_content')
-        mail.select()
-        end_date = datetime.date.today()
-        start_date = datetime.timedelta(3)
-        range_date = (end_date - start_date).strftime("%d-%b-%Y")
-        print(end_date)
-        print(start_date)
-        print(range_date)
+    def set_date(self):
+        range_date = []
+        today = datetime.date.today()
+        margin = datetime.timedelta(30)
+        start = (today - margin)
 
-        # _, data = mail.search(None, \
-        #     '(FROM "abc@nicepay.co.kr" \
-        #     SINCE \"{}\")'.format(datetime.date.today()))
+        range_date.append(today.strftime("%d-%b-%Y"))
+        range_date.append(start.strftime("%d-%b-%Y"))
+        # print(range_date)
+        return range_date
+
+    def get_electric_charge(self, mail):
+        mail.select()
+        range_date = self.set_date()
 
         _, data = mail.search(None, \
-            '(SENTSINCE {})'.format(range_date), \
-            '(FROM "arisuyogm@i121.seoul.go.kr")')
+            '(SINCE {} BEFORE {} \
+            FROM "billing_info@kepco.co.kr")'\
+            .format(range_date[1], range_date[0]))
 
         mail_ids = data[0]
         id_list = mail_ids.split()
-        latest_id_list = id_list[-1:]
+
+        for each in id_list:
+            _, dat = mail.fetch(each, '(RFC822)')
+            msg = email.message_from_bytes(dat[0][1])
+
+            while msg.is_multipart():
+                msg = msg.get_payload(0)
+            
+            content = msg.get_payload(decode=True)
+            content = content.decode('utf-8')
+            fee = self.parse_fee('E', content)
+            print(fee)
+
+    def get_water_charge(self, mail):
+        mail.select()
         
-        for each in latest_id_list:
+        _, data = mail.search(None, \
+            '(SINCE {} BEFORE {} \
+            FROM "arisuyogm@i121.seoul.go.kr")'\
+            .format("12-Nov-2020", "12-Jan-2021"))
+
+        mail_ids = data[0]
+        id_list = mail_ids.split()
+        
+        for each in id_list:
             print('for loop')
             _, dat = mail.fetch(each, '(RFC822)')
             msg = email.message_from_bytes(dat[0][1])
@@ -49,16 +74,33 @@ class EmailModule():
             content = msg.get_payload(decode=True)
             content = content.decode('utf-8')
             # print(content.decode('utf-8'))
+            fee = self.parse_fee('W', content)
+            print(fee)
 
-            soup = BeautifulSoup(content, 'html.parser')
-            spans = soup.find_all('span')
-            print(spans[4].get_text().replace(',', '').replace('원', ''))
+    def parse_fee(self, charge_type, content):
+        soup = BeautifulSoup(content, 'html.parser')
+
+        spans = soup.find_all('span')
+        if charge_type == 'E':
+            print('electric')
+            span = spans[7]
+        elif charge_type == 'W':
+            print('water')
+            span = spans[4]
+        else:
+            print('communication')
+
+        charge = span.get_text()
+        korean = re.compile('[\u3131-\u3163\uac00-\ud7a3\s,]+')
+        line = re.sub(korean, '', charge)
+        return line
 
 
 def main():
     em = EmailModule()
     mail = em.login()
-    em.read_content(None, mail)
+    em.get_water_charge(mail)
+    em.get_electric_charge(mail)
 
 if __name__ == "__main__":
     main()
