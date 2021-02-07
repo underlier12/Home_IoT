801c2d34d568d309f3b2a6c4205f655afc8ebbc1
wychoi <underlier12@gmail.com>
2021-02-07 11:34:35 +0900

    feat: Add argument and remove file after read data

--- crontab.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source /home/wy.choi/workspace/Home_IoT/venv/bin/activate
-python /home/wy.choi/workspace/Home_IoT/temp_humi_dashboard/main.py g
--- dashboard/mail.py
+++ dashboard/mail.py
@@ -6,14 +6,28 @@ import datetime
 import re
 import base64
 import pdfplumber
+import sys
+import argparse
 
 from bs4 import BeautifulSoup
+from datetime import date
 
 class EmailModule():
-    def __init__(self):
+    def __init__(self, target=None):
         self.user = 'user@naver.com'
         self.pswd = 'pswd'
         self.ATTCH = 'attachments'
+        self.d_day = self.adapt_date(target)
+
+    def adapt_date(self, tg):
+        if tg is not None:
+            # print(tg)
+            d_day = date(int(tg[0]), int(tg[1]), 1)
+            # print(d_day)
+        else:
+            margin = datetime.timedelta(30)
+            d_day = date.today() - margin
+        return d_day
 
     def login(self):
         mail = imaplib.IMAP4_SSL("imap.naver.com", 993)
@@ -28,36 +42,37 @@ class EmailModule():
             "ktbill@kt-bill.kt.com"
         ]
         mail.select()
+        range_date = self.set_date()
+
         for sent in sent_list:
-            id_list = self.search_data(mail, sent)
+            id_list = self.search_data(mail, sent, range_date)
             content = self.pluck_content(mail, id_list, sent)
             fee = self.parse_fee(sent[0], content)
             charge_list.append(fee)
-
-        print(charge_list)
+        # print(charge_list)
         return charge_list
 
-    def search_data(self, mail, sent):
-        range_date = self.set_date()
+    def set_date(self):
+        range_date = []
+        start = self.d_day
+        # today = datetime.date.today()
+        margin = datetime.timedelta(30)
+        end = (start + margin)
 
+        range_date.append(start.strftime("%d-%b-%Y"))
+        range_date.append(end.strftime("%d-%b-%Y"))
+        print(range_date)
+        return range_date
+
+    def search_data(self, mail, sent, range_date):
         _, data = mail.search(None, \
             '(SINCE {} BEFORE {} FROM {})'\
-            .format(range_date[1], range_date[0], sent))
+            .format(range_date[0], range_date[1], sent))
 
         mail_ids = data[0]
         id_list = mail_ids.split()
         return id_list
 
-    def set_date(self):
-        range_date = []
-        today = datetime.date.today()
-        margin = datetime.timedelta(30)
-        start = (today - margin)
-
-        range_date.append(today.strftime("%d-%b-%Y"))
-        range_date.append(start.strftime("%d-%b-%Y"))
-        # print(range_date)
-        return range_date
 
     def pluck_content(self, mail, id_list, sent):
         if id_list:
@@ -105,36 +120,44 @@ class EmailModule():
         with pdfplumber.open(path, password='pswd') as pdf:
             page = pdf.pages[0]
             text = page.extract_text()
+        os.remove(path)
         return text
 
     def parse_fee(self, charge_type, content):
         if charge_type == 'k':
             place_charge = content.find('월 요금')
-            print(place_charge)
+            # print(place_charge)
             charge = content[place_charge+4:place_charge+14]
             print(charge)
         else:
+            if content is None:
+                return 0
             soup = BeautifulSoup(content, 'html.parser')
             spans = soup.find_all('span')
             if charge_type == 'b':
                 span = spans[7]
             elif charge_type == 'a':
-                try:
-                    span = spans[4]
-                except:
-                    span = "<div>0</div>"
+                span = spans[4]
             else:
                 print('Not adaptable charge type')
             charge = span.get_text()
-            print(charge)
+            # print(charge)
         korean = re.compile('[\u3131-\u3163\uac00-\ud7a3\s,]+')
         line = re.sub(korean, '', charge)
-        print(line)
+        # print(line)
         return int(line)
 
+def command_options():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--target', type=int,\
+        nargs='+', help='Targeted year and month')
+    return parser.parse_args()
 
 def main():
-    em = EmailModule()
+    args = command_options()
+    target = args.target
+    # print(target)
+    em = EmailModule(target)
     mail = em.login()
     charge_list = em.get_charges(mail)
     print(charge_list)
