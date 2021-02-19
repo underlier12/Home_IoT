ecb0ed8ec89a1e2a8bc7ea0d5d738d3f9e6ea643
wychoi <underlier12@gmail.com>
2021-02-19 23:58:44 +0900

    feat: Add dashboard and Upgrade with selenium

--- monitoring_server/flask_server.py
+++ monitoring_server/flask_server.py
@@ -1,8 +1,12 @@
 from flask import Flask
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
 import os
 import time
 import pyautogui
 import random
+import requests
 
 app = Flask(__name__)
 
@@ -11,17 +15,18 @@ def essential():
     os.system('displayswitch /clone')
     time.sleep(3)
 
-    # os.system('taskkill /F /IM chrome.exe')
-    # time.sleep(1)
+    driver = webdriver.Chrome('/Users/under/tools/chromedriver')
+    driver.implicitly_wait(3)
+    url = 'https://www.youtube.com/playlist?list=PLKRZTF1Q1uwbkyqPDcZDF4y-_TLUYy7r8'
+    driver.get(url)
+    complete_html = driver.page_source
+    soup = BeautifulSoup(complete_html, 'html.parser')
+    renderers = soup.find_all('a', {'class': 'ytd-playlist-video-renderer'})
+    path = renderers[random.randint(1, 10)]['href']
+    bgm = 'youtube.com' + path
 
-    os.system('start chrome --new-window --start-maximized \
-        https://www.youtube.com/playlist?list=PLKRZTF1Q1uwbkyqPDcZDF4y-_TLUYy7r8')
-    time.sleep(5)
-
-    x, y = randomset()
-    pyautogui.click(x, y)
-    time.sleep(2)
-
+    os.system('start chrome --new-window --start-maximized ' + bgm)
+    time.sleep(3)
     pyautogui.press('f')
     return 'clear'
        
@@ -31,11 +36,11 @@ def partyover():
     os.system('displayswitch /extend')
     return 'clear'
 
-def randomset():
-    x = 750
-    y = list(range(220, 1021, 100))
-    y_select = random.choice(y)
-    return x, y_select
+@app.route("/dashboard")
+def dashboard():
+    os.system('start chrome --new-window --start-maximized \
+        http://localhost:5601/app/kibana#/dashboard/bf77d170-4e46-11eb-97ef-b13db7bc9238')
+    return 'clear'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
