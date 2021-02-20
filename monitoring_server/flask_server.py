40fe78be6077c6f199798830e4e43d342a11f5b8
wychoi <underlier12@gmail.com>
2021-02-20 15:10:55 +0900

    feat: Add cafe part and refactoring

--- monitoring_server/flask_server.py
+++ monitoring_server/flask_server.py
@@ -15,19 +15,11 @@ def essential():
     os.system('displayswitch /clone')
     time.sleep(3)
 
-    driver = webdriver.Chrome('/Users/under/tools/chromedriver')
-    driver.implicitly_wait(3)
     url = 'https://www.youtube.com/playlist?list=PLKRZTF1Q1uwbkyqPDcZDF4y-_TLUYy7r8'
-    driver.get(url)
-    complete_html = driver.page_source
-    soup = BeautifulSoup(complete_html, 'html.parser')
-    renderers = soup.find_all('a', {'class': 'ytd-playlist-video-renderer'})
-    path = renderers[random.randint(1, 10)]['href']
-    bgm = 'youtube.com' + path
-
-    os.system('start chrome --new-window --start-maximized ' + bgm)
-    time.sleep(3)
-    pyautogui.press('f')
+    tag_class = 'ytd-playlist-video-renderer'
+    max_range = 10
+    bgm = selesoup(url, tag_class, max_range)
+    play(bgm)
     return 'clear'
        
 @app.route("/partyover")
@@ -36,11 +28,37 @@ def partyover():
     os.system('displayswitch /extend')
     return 'clear'
 
+@app.route("/cafe")
+def cafe():
+    url = 'https://www.youtube.com/results?search_query=cafe+jazz'
+    tag_class = 'ytd-video-renderer'
+    max_range = 30
+    bgm = selesoup(url, tag_class, max_range)
+    play(bgm)
+    return 'clear'
+
 @app.route("/dashboard")
 def dashboard():
     os.system('start chrome --new-window --start-maximized \
         http://localhost:5601/app/kibana#/dashboard/bf77d170-4e46-11eb-97ef-b13db7bc9238')
     return 'clear'
+
+def selesoup(url, tag_class, max_range):
+    driver = webdriver.Chrome('/Users/under/tools/chromedriver')
+    driver.implicitly_wait(3)
+    driver.get(url)
+    complete_html = driver.page_source
+
+    soup = BeautifulSoup(complete_html, 'html.parser')
+    renderers = soup.find_all('a', {'class': tag_class})
+    path = renderers[random.randint(1, max_range)]['href']
+    bgm = 'youtube.com' + path
+    return bgm
+
+def play(bgm):
+    os.system('start chrome --new-window --start-maximized ' + bgm)
+    time.sleep(5)
+    pyautogui.press('f')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
